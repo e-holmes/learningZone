9b012d0772bf32af60a23f27755084f517308aaa
e-holmes <bethholmes768@gmail.com>
2020-05-09 15:21:57 -0700

    I am truly stumped on how to make the dice individual buttons. Taking a break and asking for help then coming back

--- mlm/src/pages/Drawing.js
+++ mlm/src/pages/Drawing.js
@@ -7,6 +7,12 @@ class Drawing extends Component {
     state = {
         data,
         dice: data,
+        rolls: 0,
+        maybe: 0,
+        score: 0,
+        one:0,
+        two:0,
+        
     }
 
     // returns random number
@@ -17,6 +23,7 @@ class Drawing extends Component {
     }
     //  
     myDice = (data) => {
+        let k = this.state.rolls;
         let i = 0;
         var myDice = [];
         console.log(myDice);
@@ -31,12 +38,26 @@ class Drawing extends Component {
                                 : data[5].id === num ? myDice.push(data[5])
                                     : result = false;
             result ? console.log(myDice) : console.log(result);
+            myDice[i].id = i;
             i++;
         }
+
+        // this.updateID(myDice);
+
         this.setState({
-            dice: myDice
+            dice: myDice,
+            rolls: (k + 1)
         });
     }
+
+    // updateID = (myDice) => {
+    //     myDice[0].id = 1;
+    //     myDice[1].id = 2;
+    //     myDice[2].id = 3;
+    //     console.log(myDice[0]);
+    //     console.log(myDice[1]);
+    //     console.log(myDice[2]);
+    // }
 
 
     selectScore = () => {
@@ -53,25 +74,61 @@ class Drawing extends Component {
                 </section>
 
                 <section className="row d-flex justify-content-center">
-                    {this.state.dice.map(item => (
-                        <Picture
-                            key={item.id}
-                            id={item.id}
-                            image={item.image}
-                        />
-                    ))}
+                    <section className="col">
+                        {this.state.dice.map(item => (
+                            <Picture
+                                key={item.id}
+                                id={item.id}
+                                handleClick={this.handleItemClick}
+                                image={item.image}
+                            />
+                        ))}
+                    </section>
+                    <section className="col">
+                        <section className="row">
+                            <h1>Upper Section</h1>
+                            <p>
+                                Ones: {this.state.one}
+                                Twos: {this.state.two}
+                                Threes: {this.state.three}
+                                Fours: {this.state.four}
+                                Five: {this.state.five}
+                                Six: {this.state.six}
+                            </p>
+                        </section>
+                        <section className="row">
+                            <h1>Lower Section</h1>
+                            <p>
+                                Three of a Kind: {this.state.triple}
+                                Four of a Kind: {this.state.double}
+                                Full House: {this.state.house}
+                                Low Straight: {this.state.low}
+                                High Straight: {this.state.high}
+                                Yahtzee: {this.state.yahtzee}
+                                Chance: {this.state.chance}
+                            </p>
+                        </section>
+                    </section>
                 </section>
 
-                <section id="intoButton">
-                    {/* <!-- Store Button --> */}
-                    <Button
-                        click={this.myDice.bind(this, data)}
-                        text="Roll Dice"
-                    ></Button>
-                    <Button
-                        click={this.selectScore}
-                        text="Select Score"
-                    ></Button>
+                <section className="row">
+                    <section className="col">
+                        <h2> Score: {this.state.score} </h2>
+                        <h3> Score: {this.state.maybe}</h3>
+                        <h3> Rolls Used: {this.state.rolls}</h3>
+                    </section>
+
+                    <section className="col" id="intoButton">
+                        {/* <!-- Store Button --> */}
+                        <Button
+                            click={this.myDice.bind(this, data)}
+                            text="Roll Dice"
+                        ></Button>
+                        <Button
+                            click={this.selectScore}
+                            text="Select Score"
+                        ></Button>
+                    </section>
                 </section>
 
             </div>
